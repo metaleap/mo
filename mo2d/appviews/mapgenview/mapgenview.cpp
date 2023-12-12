f16415686c717314ce06170acc0a440b0d9d43f0
Phil Schumann <metaleap@users.noreply.github.com>
2023-12-12 04:02:30 +0100

    mapgen cont.

--- mo2d/appviews/mapgenview/mapgenview.cpp
+++ mo2d/appviews/mapgenview/mapgenview.cpp
@@ -1,17 +1,27 @@
 #include <cstdio>
 #include <filesystem>
+#include <map>
+
 #include <imgui.h>
 
 #include "./libnoise_utils/noiseutils.h"
 
 #include "./mapgenview.h"
 #include "perlin.h"
+#include "scalebias.h"
+
+
+
+float mix(float x, float y, float a) {
+    return (x * (1.0f - a)) + (y * a);
+}
 
 
 MapGenView::MapGenView() {
-    this->perlinNoise.SetLacunarity(2.111);
+    this->perlinNoise.SetLacunarity(2.121);
     this->perlinNoise.SetOctaveCount(30);
     this->perlinNoise.SetSeed(12);
+    this->perlinNoise.SetPersistence(0.531);
     this->finalTerrain.SetBounds(-1000, 10); // full perlin range is "usually but not guaranteed always" -1..1
     // this->finalTerrain.SetEdgeFalloff(0.11);
 
@@ -31,9 +41,6 @@ void MapGenView::onUpdate(sf::Time) {
         int seed = perlinNoise.GetSeed();
         if (ImGui::InputInt("Seed", &seed))
             perlinNoise.SetSeed(seed);
-        int num_octaves = perlinNoise.GetOctaveCount();
-        if (ImGui::InputInt("Octaves (6-30)", &num_octaves))
-            perlinNoise.SetOctaveCount(num_octaves);
         float gappiness = (float)(perlinNoise.GetLacunarity());
         if (ImGui::InputFloat("Lacunarity (Gappiness)", &gappiness))
             perlinNoise.SetLacunarity(gappiness);
@@ -71,58 +78,89 @@ void MapGenView::reGenerate(bool tiny) {
     this->finalTerrain.SetSourceModule(1, perlinNoise);
     this->finalTerrain.SetControlModule(perlinNoise);
 
-    utils::NoiseMap heightMap;
-    utils::NoiseMapBuilderSphere heightMapBuilder;
-    heightMapBuilder.SetSourceModule(perlinNoise);
-    heightMapBuilder.SetDestNoiseMap(heightMap);
-    heightMapBuilder.SetDestSize(tiny ? 512 : 2048, tiny ? 256 : 1024);
-    heightMapBuilder.SetBounds(-90.0, 90.0, -180.0, 180.0);
-    heightMapBuilder.Build();
-    clock_t t_end = clock();
-    printf("heights done: %fsec\n", (float)(t_end - t_start) / CLOCKS_PER_SEC);
+    module::ScaleBias scaler;
+    scaler.SetSourceModule(0, perlinNoise);
+    scaler.SetScale(0.7);
 
-    utils::RendererImage renderer;
-    renderer.SetSourceNoiseMap(heightMap);
-    { // coloring
-        renderer.ClearGradient();
-        renderer.AddGradientPoint(-1.111, utils::Color(0, 0, 128, 255));   // very-deeps
-        renderer.AddGradientPoint(-0.220, utils::Color(0, 0, 255, 255));   // water
-        renderer.AddGradientPoint(-0.011, utils::Color(0, 128, 255, 255)); // shoal
-        renderer.AddGradientPoint(-0.001, utils::Color(0, 128, 255, 255)); // shoal
-        renderer.AddGradientPoint(0.000, utils::Color(128, 128, 32, 255)); // sand
-        renderer.AddGradientPoint(0.002, utils::Color(128, 160, 0, 255));  // grass
-        renderer.AddGradientPoint(0.125, utils::Color(32, 160, 0, 255));   // grass
-        renderer.AddGradientPoint(0.375, utils::Color(128, 128, 128, 255));
-        renderer.AddGradientPoint(0.750, utils::Color(196, 196, 196, 255));
-        renderer.AddGradientPoint(1.111, utils::Color(255, 255, 255, 255));
-        renderer.EnableLight();
-        renderer.SetLightContrast(3.21);
-        renderer.SetLightBrightness(2.34);
+    for (const auto &is_tile : std::map<bool, bool> {{true, true}, {false, false}}) {
+        if (is_tile.first)
+            continue;
+        utils::NoiseMap heightMap;
+        utils::NoiseMapBuilderSphere heightMapBuilder;
+        heightMapBuilder.SetSourceModule(scaler);
+        heightMapBuilder.SetDestNoiseMap(heightMap);
+        heightMapBuilder.SetDestSize(tiny ? 512 : 2048, tiny ? 256 : 1024);
+        heightMapBuilder.SetBounds(-90.0, 90.0, -180.0, 180.0);
+        if (is_tile.first) {
+            heightMapBuilder.SetDestSize(2048, 2048);
+            heightMapBuilder.SetBounds(-1, 0, -1, 0);
+        }
+        heightMapBuilder.Build();
+        clock_t t_end = clock();
+        for (int y = 0, h = heightMap.GetHeight(), y_h = h / 8; y < y_h; y++) {
+            const float weight = sqrt(sqrt(((float)y / (float)y_h))); // (0 -> force ocean) .. (1 -> keep as-is)
+            for (int x = 0, w = heightMap.GetWidth(); x < w; x++) {
+                auto value = heightMap.GetValue(x, y);
+                heightMap.SetValue(x, y, mix(-1, value, weight));
+                value = heightMap.GetValue(x, h - y);
+                heightMap.SetValue(x, h - y, mix(-1, value, weight));
+            }
+        }
+        float max = -1;
+        if (!tiny)
+            for (int x = 0, w = heightMap.GetWidth(); x < w; x++)
+                for (int y = 0, h = heightMap.GetWidth(); y < h; y++) {
+                    const float value = heightMap.GetValue(x, y);
+                    if (value > max)
+                        max = value;
+                }
+        printf("heights done: max=%f, %fsec\n", max, (float)(t_end - t_start) / CLOCKS_PER_SEC);
+
+        utils::RendererImage renderer;
+        renderer.SetSourceNoiseMap(heightMap);
+        { // coloring
+            renderer.ClearGradient();
+            renderer.AddGradientPoint(-1.111, utils::Color(0, 0, 128, 255));   // very-deeps
+            renderer.AddGradientPoint(-0.220, utils::Color(0, 0, 255, 255));   // water
+            renderer.AddGradientPoint(-0.011, utils::Color(0, 128, 255, 255)); // shoal
+            renderer.AddGradientPoint(-0.001, utils::Color(0, 128, 255, 255)); // shoal
+            renderer.AddGradientPoint(0.000, utils::Color(128, 128, 32, 255)); // sand
+            renderer.AddGradientPoint(0.002, utils::Color(128, 160, 0, 255));  // grass
+            renderer.AddGradientPoint(0.125, utils::Color(32, 160, 0, 255));   // grass
+            renderer.AddGradientPoint(0.375, utils::Color(128, 128, 128, 255));
+            renderer.AddGradientPoint(0.750, utils::Color(196, 196, 196, 255));
+            renderer.AddGradientPoint(1.111, utils::Color(244, 244, 244, 255));
+            renderer.EnableLight();
+            renderer.SetLightContrast(3.21);
+            renderer.SetLightBrightness(2.34);
+        }
+        utils::Image image;
+        renderer.SetDestImage(image);
+        t_start = clock();
+        renderer.Render();
+
+        const auto out_file_path = std::filesystem::absolute(
+            "../.local/temp_" + std::string(tiny ? "tiny" : (is_tile.first ? "tile" : "full")) + ".bmp");
+        utils::WriterBMP writer;
+        writer.SetSourceImage(image);
+        writer.SetDestFilename(out_file_path);
+        writer.WriteDestFile();
+        t_end = clock();
+
+        if (tiny) {
+            this->previewTinyTex.loadFromFile(out_file_path);
+            this->previewTinyRect.setTexture(&this->previewTinyTex);
+            const auto size_tex = this->previewTinyTex.getSize();
+            this->previewTinyRect.setTextureRect(sf::IntRect {0, 0, (int)(size_tex.x), (int)(size_tex.y)});
+        } else if (!is_tile.first) {
+            this->previewFullTex.loadFromFile(out_file_path);
+            this->previewFullRect.setTexture(&this->previewFullTex);
+            const auto size_tex = this->previewFullTex.getSize();
+            this->previewFullRect.setTextureRect(sf::IntRect {0, 0, (int)(size_tex.x), (int)(size_tex.y)});
+            this->reGenerate(true);
+        }
+
+        printf("%s: %fsec\n", out_file_path.c_str(), (float)(t_end - t_start) / CLOCKS_PER_SEC);
+        fflush(stdout);
     }
-    utils::Image image;
-    renderer.SetDestImage(image);
-    t_start = clock();
-    renderer.Render();
-
-    const auto out_file_path = std::filesystem::absolute("../.local/temp_" + std::string(tiny ? "tiny" : "full") + ".bmp");
-    utils::WriterBMP writer;
-    writer.SetSourceImage(image);
-    writer.SetDestFilename(out_file_path);
-    writer.WriteDestFile();
-    t_end = clock();
-
-    if (tiny) {
-        this->previewTinyTex.loadFromFile(out_file_path);
-        this->previewTinyRect.setTexture(&this->previewTinyTex);
-        const auto size_tex = this->previewTinyTex.getSize();
-        this->previewTinyRect.setTextureRect(sf::IntRect {0, 0, (int)(size_tex.x), (int)(size_tex.y)});
-    } else {
-        this->previewFullTex.loadFromFile(out_file_path);
-        this->previewFullRect.setTexture(&this->previewFullTex);
-        const auto size_tex = this->previewFullTex.getSize();
-        this->previewFullRect.setTextureRect(sf::IntRect {0, 0, (int)(size_tex.x), (int)(size_tex.y)});
-        this->reGenerate(true);
-    }
-
-    printf("image done: %fsec\n", (float)(t_end - t_start) / CLOCKS_PER_SEC);
 }
